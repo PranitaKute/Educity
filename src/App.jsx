657c8119e4f9389250f617d5cbef3dad130e7b92
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-02-28 01:41:38 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/NavBar/navbar'
+import Hero from './components/Hero/Hero'
+
+const App= () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
