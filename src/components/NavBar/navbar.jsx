92184dea3c6ed6baecf8de171aa133d087a637b4
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-03-04 16:24:38 +0530

    Update navbar.jsx

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -3,6 +3,16 @@ import './navbar.css'
 import logo from '../../assets/logo.png'
 
 const Navbar = () => {
+
+  const [sticky, setSticky] = useState(false);
+
+    useEffect(()=>{
+      window.addEventListener('scroll',()=>{
+        window.scrollY > 50 ? setSticky(true) : setSticky(false);
+      })
+    },[]);
+
+  
   return (
     <nav className='container'>
       <img src={logo} alt='' className='logo'/>
