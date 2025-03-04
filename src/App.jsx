deab8898bcc37468c3e9efb954a5be37e3f54e5d
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-03-04 16:54:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import Navbar from './components/NavBar/navbar'
 import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/program'
+import Title from './components/Title/Title'
+import About from './components/About/About'
+import Campus from './components/Campus/Campus'
 
 const App= () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      
+      <div className="container">
+        <Title subtitle ='Our Programs' title='What We Offer'/>
+        <Programs/>
+        <About/>
+        <Title subtitle ='Gallery' title='Campus Photos'/>
+        <Campus/>
+        <Title subtitle ='TESTIMONIALS' title='What Students Says'/>
+      </div>
+      
     </div>
   )
 }
