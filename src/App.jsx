703a910974c1c75b6764f1f74fff8ca84aaf69f5
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-03-06 23:31:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from './components/About/About'
 import Campus from './components/Campus/Campus'
 import Testimonials from './components/Testimonials/Testimonials'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 const App= () => {
   return (
@@ -24,6 +25,7 @@ const App= () => {
         <Testimonials/>
         <Title subtitle ='Contact Us' title='Get In Touch'/>
         <Contact/>
+        <Footer/>
       </div>
       
     </div>
