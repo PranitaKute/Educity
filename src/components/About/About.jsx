bbdea4ef61614df3394e214d040023dc5c94070d
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-03-07 14:11:04 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,12 +3,12 @@ import './About.css'
 import about_img from '../../assets/about.png'
 import play_icon from '../../assets/play-icon.png'
 
-const About = () => {
+const About = ({setPlayState}) => {
   return (
     <div className='about'>
       <div className="about-left">
         <img src={about_img} alt="" className='about_img' />
-        <img src={play_icon} alt="" className='play_icon' />
+        <img src={play_icon} alt="" className='play_icon' onClick={() => setPlayState(true)} />
       </div>
 
       <div className="about-right">
@@ -22,7 +22,7 @@ const About = () => {
         <p>With a focus on innovation, hands-on learning, and personalized
             mentorship, our programs prepare aspiring educators to make a 
             meaningful impact in classrooms, schools, and communities.</p>
-        <p>Whether you aspire to become a teacher, administrator, cpunselor, or 
+        <p>Whether you aspire to become a teacher, administrator, counselor, or 
             educational leader, our diverse range of programs offers the 
             perfect pathway to achieve your goals and unlock your full potential 
             in shaping the future of education. </p>
