ff36028584e895b087d06e73bff6739166ba5ec6
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-03-07 14:10:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { useState } from 'react';
+import React from 'react';
 import Navbar from './components/NavBar/navbar'
 import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/program'
@@ -8,25 +9,28 @@ import Campus from './components/Campus/Campus'
 import Testimonials from './components/Testimonials/Testimonials'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
+import VideoPlayer from './components/VideoPlayer/VideoPlayer'
 
 const App= () => {
+  const [playState, setPlayState] = useState(false);
+
   return (
     <div>
       <Navbar/>
       <Hero/>
-      
       <div className="container">
         <Title subtitle ='Our Programs' title='What We Offer'/>
         <Programs/>
-        <About/>
+        <About setPlayState={setPlayState}/>
         <Title subtitle ='Gallery' title='Campus Photos'/>
         <Campus/>
-        <Title subtitle ='TESTIMONIALS' title='What Students Says'/>
+        <Title subtitle ='TESTIMONIALS' title='What Students Say'/>
         <Testimonials/>
         <Title subtitle ='Contact Us' title='Get In Touch'/>
         <Contact/>
         <Footer/>
       </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState}/>
       
     </div>
   )
